b5942dde0c4dee2b30977315cfed8c05f0c68700
Mihai <mihaitu.popescu@gmail.com>
2018-02-05 17:38:35 +0100

    did it for 10 times

--- index1.c
+++ index1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    printf("Hello world\n");
+    for (int i = 0; i < 10; i++) {
+        printf("Hello world!\n");
+    }
+    return 0;
 }
