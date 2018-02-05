05aacb3df1b9c3abb2fa7380fa0778182af67acb
Mihai <mihaitu.popescu@gmail.com>
2018-02-05 18:31:21 +0100

    behaviors self.state interactions

--- behaviour.py
+++ behaviour.py
@@ -1,3 +1,33 @@
+import basebehavior.behaviorimplementation
+
+
+class Mymainbehavior_x(basebehavior.behaviorimplementation.BehaviorImplementation):
+    '''this is a behavior implementation template'''
+    #this implementation should not define an __init__ !!!
+    def implementation_init(self):
+        #CALLED ONCE
+        #define list of sub-behavior here
+        self.createSubbehavior()
+        self.startMySubbehavior = False
+        self.selected_behaviors = [('mySubBehavior', "self.startMySubbehavior == True")]
+        self.state = 'idle'
+    
+    def createSubbehavior(self):
+        self.startMySubbehavior = False
+        self.mySubBehavior = self.ab.mysubbehavior({})
+
+    def implementation_update(self):
+        #CALLED at 10Hz
+        #check what behaviors have failed or finished and do other things when something has failed 
+        if self.state == 'idle':
+            self.startMySubbehavior = True
+            self.state = 'runningMySubbehavior'
+            print "starting my subbehavior.."
+        elif self.mySubBehavior.is_failed():
+            print self.mySubBehavior.get_failure_reason()
+            self.createSubbehavior()
+            print "my subbehavior failed"
+            self.state = 'idle'
 [mybehaviour]
 description = "It does things you can't even dream of"
 postcondition = False
