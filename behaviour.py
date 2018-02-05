007ef7b91390859af78ca8645f15c4207ddaecb7
Mihai <mihaitu.popescu@gmail.com>
2018-02-05 17:53:04 +0100

    Added behaviour and sub behaviour

---
+++ behaviour.py
@@ -0,0 +1,8 @@
+[mybehaviour]
+description = "It does things you can't even dream of"
+postcondition = False
+
+[mysubbehaviour]
+description = "It fixes the main behaviour"
+postcondition = False
+
